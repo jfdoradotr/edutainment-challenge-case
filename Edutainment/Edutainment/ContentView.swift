e9589b3834fb68fbad2ce87f2e75b7b8bff80f00
Juan Dorado <jfdoradotr@icloud.com>
2024-12-10 21:21:34 -0600

    Create the user selection to select the table to practice and the amount of questions

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -5,10 +5,27 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var tableValue = 2
+  @State private var amountOfQuestionsSelected = 5
+
+  private let amountOfQuestions = [5, 10, 20]
+
   var body: some View {
     NavigationStack {
       List {
-
+        Section(header: Text("Select your table")) {
+          Stepper(value: $tableValue, in: 2...12, step: 1) {
+            Text("\(tableValue)")
+          }
+        }
+        Section(header: Text("Select the amount of questions")) {
+          Picker("Amount of questions", selection: $amountOfQuestionsSelected) {
+            ForEach(amountOfQuestions, id: \.self) { amountOfQuestions in
+              Text("\(amountOfQuestions)")
+            }
+          }
+          .pickerStyle(.segmented)
+        }
       }
       .navigationTitle("Edutainment")
     }
