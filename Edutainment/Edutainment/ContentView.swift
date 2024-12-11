3d1578fb7b3665ad8c37e50e2ca6a4d78b663a04
Juan Dorado <jfdoradotr@icloud.com>
2024-12-10 21:40:16 -0600

    Show/hide the playing section

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -7,7 +7,7 @@ import SwiftUI
 struct ContentView: View {
   @State private var tableValue = 2
   @State private var amountOfQuestionsSelected = 5
-  @State private var isPlaying = true
+  @State private var isPlaying = false
   @State private var valueToMultiply = 0
   @State private var multiplicationAnswer = ""
   @State private var questionCounter = 1
@@ -17,32 +17,40 @@ struct ContentView: View {
   var body: some View {
     NavigationStack {
       List {
-        Section(header: Text("Select your table")) {
-          Stepper(value: $tableValue, in: 2...12, step: 1) {
-            Text("\(tableValue)")
-          }
-        }
-        Section(header: Text("Select the amount of questions")) {
-          Picker("Amount of questions", selection: $amountOfQuestionsSelected) {
-            ForEach(amountOfQuestions, id: \.self) { amountOfQuestions in
-              Text("\(amountOfQuestions)")
+        Group {
+          Section(header: Text("Select your table")) {
+            Stepper(value: $tableValue, in: 2...12, step: 1) {
+              Text("\(tableValue)")
             }
           }
-          .pickerStyle(.segmented)
-        }
-        Section(header: Text("Question \(questionCounter)/\(amountOfQuestionsSelected)")) {
-          HStack {
-            Text("\(tableValue) X \(valueToMultiply) = ")
-            TextField("Answer", text: $multiplicationAnswer)
+          Section(header: Text("Select the amount of questions")) {
+            Picker("Amount of questions", selection: $amountOfQuestionsSelected) {
+              ForEach(amountOfQuestions, id: \.self) { amountOfQuestions in
+                Text("\(amountOfQuestions)")
+              }
+            }
+            .pickerStyle(.segmented)
           }
-          .font(.largeTitle)
+        }
+        .disabled(isPlaying)
+
+        if isPlaying {
+          Section(header: Text("Question \(questionCounter)/\(amountOfQuestionsSelected)")) {
+            HStack {
+              Text("\(tableValue) X \(valueToMultiply) = ")
+              TextField("Answer", text: $multiplicationAnswer)
+            }
+            .font(.largeTitle)
+          }
         }
       }
       .navigationTitle("Edutainment")
       .toolbar {
         ToolbarItem(placement: .primaryAction) {
           Button("Start") {
-
+            withAnimation {
+              isPlaying.toggle()
+            }
           }
         }
       }
